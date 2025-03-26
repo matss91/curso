c032673b5a2110718fc73b1689dd99494134f2af
matias cardillo <matcardillo91@gmail.com>
2025-03-25 21:41:31 -0300

    backend funcionando

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+const connectDB = require('./config/db');
 var app = express();
-
+connectDB();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -19,8 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +37,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(8000, ()=> {
+  console.log(`Server running in https://localhost:8000`);
+  });
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const { 
+  list,store,detail,upDate,remove
+ } = require('../controllers/projectsController');
+ /* GET home page. */
+ 
+ router.get('/projects',list);
+ router.post('/projects',store);
+ 
+ 
+ router.get('/projects/:id',detail);
+ router.put('/projects/:id',upDate);
+ router.delete('/projects/:id',remove);
 
 module.exports = router;
